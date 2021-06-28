f1f3927885bf6d8246ef7b301f524d0aacac1aa5
翁子皓 <x142y857@gmail.com>
2021-06-28 14:03:52 +0800

    13-3

--- Player.kt
+++ Player.kt
@@ -1,17 +1,15 @@
 class Player(_name: String,
-             _healthPoints: Int,
-             _isBlessed: Boolean,
-             _isImmortal: Boolean) {
+             var =healthPoints: Int,
+             val =isBlessed: Boolean,
+             private val =isImmortal: Boolean) {
 
-        var name =_name
+    var name = _name
         get() = field.capitalize()
-        private set (value) {
+        private set(value) {
             field = value.trim()
         }
 
-    var healthPoints =_healthPoints
-    var isBlessed =_isBlessed
-    private val isImmortal =_isImmortal
+}
 
     fun auraColor(): String {
         val auraVisible = isBlessed && healthPoints > 50 || isImmortal
